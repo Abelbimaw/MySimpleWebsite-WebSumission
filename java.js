f75be06928cc199d21ebcc891e27e2dc6cf30a2b
Abelbimaw <abelwiratama@gmail.com>
2021-02-22 13:57:45 +0700

    Final

--- java.js
+++ java.js
@@ -1,3 +1,15 @@
-function pencetfoto{
-    
+function tambah(){
+    if( document.getElementById('count').innerText < 10){
+        document.getElementById('count').innerText++;
+    } else{
+        alert("Luar biasa, anda sangat sayang Kediri :)");
+    }
+}
+
+function resik(){
+    if(confirm("Apakah anda ingin menghapus penilaian :( ?")){
+        document.getElementById('count').innerText=0;
+    } else {
+        alert("Tidak ada penghapusan");
+    }
 }
